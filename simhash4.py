d32a0ad994be91f2e63237561550031c5ff20213
guybi <guyworkprg@gmail.com>
2016-12-18 17:47:03 +0200

    improve simhash algorithm, i added Jaccard test, we get accuracy of 0.7 - its not good - need to find way to improve it to 0.8.

--- simhash4.py
+++ simhash4.py
@@ -6,17 +6,18 @@ import pickle
 import time
 import string
 
-data_file = 'Train20.csv'
+data_file = 'Train.csv'
 data_file_test = 'Test.csv'
 binary_size = 32
 maxShingleID = 2 ** binary_size - 1
 bigPrime = 4294967311
 hashfunc_num = 50
-docs_num = 100
+docs_num = 4000
 
 number_new_buckets = 0
 number_added_to_buckets = 0
 
+miss = 0
 train = "TRAIN"
 test = "TEST"
 
@@ -27,7 +28,7 @@ def read_data():
     print 'read data...'
 
     df = pd.read_csv(data_file)
-    df = df[['FullDescription']][:docs_num]
+    df = df[['FullDescription']][:1000]
 
     t = time.time() - t0
     print 'time elapsed:', t
@@ -43,15 +44,13 @@ def rand_coeffs():
 
 def doc_to_shingles(doc):
 
-    docAsShingleSets = []
-    docAsHasedShingleSets = []
     doc = doc.lower().translate(None, string.punctuation)
 
     doc_split = doc.split(" ")
 
     shingle_list = [" ".join(doc_split[i:i + 5]) for i in range(len(doc_split) - 5 + 1)]
 
-    hashed_shingle_list = [hash(single_shingle) for single_shingle in shingle_list]
+    hashed_shingle_list = [(hash(single_shingle) & 0xffffffff) for single_shingle in shingle_list]
     docAsHasedShingleSets = set(hashed_shingle_list)
 
     return docAsHasedShingleSets
@@ -60,37 +59,16 @@ def doc_to_shingles(doc):
 
 def createSignature(sdoc, A, B):
     sdoc = np.array(list(sdoc))
-    return np.min((A[:,np.newaxis]*sdoc+1*B[:,np.newaxis])%bigPrime, axis=1)
-
+    x = np.min((A[:,np.newaxis]*sdoc+1*B[:,np.newaxis])%bigPrime, axis=1)
+    # print x
+    return x
+############################################################
 def shingle_to_binary_mat(shingles):
     bin_mat = np.zeros([len(shingles), binary_size], dtype=int)
     to_bin = [bin(shingle)[2:].zfill(32) for shingle in shingles]
     bin_mat = np.array([np.fromstring(v, dtype='u1') - ord('0') for v in to_bin])
-    return np.sum(bin_mat == 0, axis=0) < np.sum(bin_mat == 1, axis=0).astype(int)
+    return (np.sum(bin_mat == 0, axis=0) < np.sum(bin_mat == 1, axis=0)).astype(int)
 
-# ############################################################
-#
-# def compare_docs(a, b):
-#     a_str = np.uint32(np.array(list(str(a))))
-#     b_str = np.uint32(np.array(list(str(b))))
-#     diff_c = 0
-#     for k in range(1, int(binary_size/4)):
-#         aBitK = a_str[-k]
-#         bBitK = b_str[-k]
-#         if aBitK != bBitK:
-#             diff_c += 1
-#         if diff_c > 1:
-#             return False
-#     for k in range(int((binary_size/4)+1), binary_size+1):
-#         aBitK = a_str[-k]
-#         bBitK = b_str[-k]
-#         if aBitK != bBitK:
-#             diff_c += 1
-#         if diff_c > 1:
-#             return False
-#     return True
-#
-# ############################################################
 ############################################################
 
 def compare_docs(a, b):
@@ -108,7 +86,6 @@ def compare_docs(a, b):
 
 ############################################################
 
-
 def addToDic(key, lineNumber):
     dicIDsignature.update({key: lineNumber})
 
@@ -118,7 +95,7 @@ def jaccard(a, b):
     seta = doc_to_shingles(df.loc[dicIDsignature[a], 'FullDescription'])
     setb = doc_to_shingles(df.loc[dicIDsignature[b], 'FullDescription'])
     n = len(seta.intersection(setb))
-    return n / float(len(seta) + len(setb) - n)
+    return n / (float(len(seta) + len(setb) - n))
 
 ############################################################
 def printDocs(sign1, sign2,status):
@@ -129,57 +106,60 @@ def printDocs(sign1, sign2,status):
         print "doc SIGN:\n",doc
 ############################################################
 def findBuckets(buckets, signature, lineNumber, status, error_num):
+    # print signature
     key = ''.join(map(str, signature))
 
-    if len(dicIDsignature) == 0: # if buckets number is 0 - need to add the sign to dicIDsignatue - open new bucket
-        print dicIDsignature
-        addToDic(key, lineNumber)   #     dicIDsignature.update({key: lineNumber})
-        buckets.setdefault(key,[]).append(lineNumber)    #[key].append(lineNumber)          # a.setdefault("somekey",[]).append("bob")
-        print "bucket tatus", buckets.keys()
-        return buckets, error_num
+    # if len(dicIDsignature) == 0: # if buckets number is 0 - need to add the sign to dicIDsignatue - open new bucket
+    #     #print dicIDsignature
+    #     addToDic(key, lineNumber)   #     dicIDsignature.update({key: lineNumber})
+    #     buckets.setdefault(key,[]).append(lineNumber)    #[key].append(lineNumber)          # a.setdefault("somekey",[]).append("bob")
+    #     return buckets, error_num
+
+
 
     if status == "TRAIN":
         addToDic(key, lineNumber)
     elif status == "TEST":
         addToDic(key, lineNumber-docs_num)
 
-
-    print "bucket tatus" , buckets.keys()
-    i = 0
+    flag = 0
+    flagJac = 0
     for bucket_key in buckets.keys():
-        flag = 0
         jac = jaccard(bucket_key, key)  # need get 0.8  for insert to excist buecket
-        res_compare_to = compare_docs(bucket_key, key)
-        # print jac
-        print "----------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FIRST", jac
-        print "----------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", res_compare_to
-        print "----------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", i
-        i += 1
-        printDocs(bucket_key, key, status)
         if jac >= 0.8:
-            flag = 1
-            print "sign1 -> Bucket [",bucket_key,"]"
-            print "sign2 -> newSign[",key,"]"
+            flagJac = 1
+            break
 
-        if res_compare_to:  # need compare
-            if jac < 0.8:
-                error_num += 1
-            buckets[bucket_key].append(lineNumber)
-            print "************* Insert to buckets -> NEED TO BE > 0.8 ACCORDING TO JACCARD *********", jac
-            if flag == 0:
-                print "sign1 -> Bucket [",bucket_key,"]"
-                print "sign2 -> newSign[",key,"]"
-            print "------------------------------------------------------------------------------------------"
+    if key in buckets:
+        buckets[key].append(lineNumber)
 
-            if(status == "TEST"):
-                global number_added_to_buckets
-                number_added_to_buckets += 1
-            return buckets, error_num
-    buckets[key] = []
-    buckets[key].append(lineNumber)
-    if(status == "TEST"):
-        global number_new_buckets
-        number_new_buckets += 1
+
+    # for bucket_key in buckets.keys():
+    #     # jac = jaccard(bucket_key, key)  # need get 0.8  for insert to excist buecket
+    #     res_compare_to = compare_docs(bucket_key, key)
+    #
+    #     if res_compare_to:  # need compare
+    #         # if jac < 0.8:
+    #         #     error_num += 1
+    #
+    #         buckets[bucket_key].append(lineNumber)
+    #         # printDocs(bucket_key, key, status)
+    #         return buckets, error_num
+    #
+    #     # if jac >= 0.8:
+    #     #     print "NOT FOUND NOT FOUND NOT FOUND NOT FOUND"
+    #     #     # printDocs(bucket_key, key, status)
+    #     #     global miss
+    #     #     miss += 1
+    #     #     error_num += 1
+    #     #     # print "---------------------------------------"
+    else:
+        flag = 1
+        buckets[key] = [lineNumber]
+
+    if flag == 1:
+        if flagJac == 1:
+            print "NOT FOUND NOT FOUND NOT FOUND NOT FOUND"
 
     return buckets, error_num
 
@@ -214,27 +194,25 @@ if __name__ == "__main__":
 
     i = 0
     for doc in docs:
-        print "\n\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=DOC : ",i
         i += 1
         iter_counter += 1
         t0 = time.time()
-        sdoc = doc_to_shingles(doc)
-        signature = createSignature(sdoc, A, B)
-        lsh_signature = shingle_to_binary_mat(signature).astype(int)
+        shingles_hash_set = doc_to_shingles(doc)   # return set hash shingles
+        signature = shingle_to_binary_mat(shingles_hash_set).astype(int)
 
-        buckets, error_num = findBuckets(buckets, lsh_signature, lineNumber, train, error_num)
+        buckets, error_num = findBuckets(buckets, signature, lineNumber, train, error_num)
         lineNumber += 1
         error = error_num / iter_counter
         accuracy = 1 - error
 
         t = time.time() - t0
         if iter_counter % print_counter == 0:
+            print "ITER #", i
             print 'time elapsed: ', t / docs_num
             print 'accuracy:', accuracy
 
     print buckets
     print 'accuracy:', accuracy
-
     # export buckets to pickle file
     pickle.dump(buckets, open('train.pkl', 'wb'))
 
@@ -268,6 +246,6 @@ if __name__ == "__main__":
     print 'accuracy:', accuracy
     print "NEW BUCKETS IN TEST : ", number_new_buckets
     print "ADDED TO BUCKETS IN TEST: ", number_added_to_buckets
-
+    print "MISS" , miss
     # export buckets to pickle file
     pickle.dump(buckets, open('test.pkl', 'wb'))
